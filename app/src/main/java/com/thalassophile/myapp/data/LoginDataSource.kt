ea3cb27b52ef0df25478d3d662097d6a0b97abe4
ThalassophilicGirl <nada.ab9969@gmail.comm>
2023-01-10 00:11:54 +0200

    modified code

--- app/src/main/java/com/thalassophile/myapp/data/LoginDataSource.kt
+++ app/src/main/java/com/thalassophile/myapp/data/LoginDataSource.kt
@@ -3,23 +3,56 @@ package com.thalassophile.myapp.data
 import com.thalassophile.myapp.data.model.LoggedInUser
 import java.io.IOException
 
+
 /**
  * Class that handles authentication w/ login credentials and retrieves user information.
  */
 
+
 class LoginDataSource {
 
     fun login(username: String, password: String): Result<LoggedInUser> {
-        try {
-            // TODO: handle loggedInUser authentication
-            val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), "Jane Doe")
-            return Result.Success(fakeUser)
+        return try {
+            // handle loggedInUser authentication
+            val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), "Sebastien Doe")
+            Result.Success(fakeUser)
+
         } catch (e: Throwable) {
-            return Result.Error(IOException("Error logging in", e))
+            Result.Error(IOException("Error logging in", e))
         }
     }
 
-    fun logout() {
-        // TODO: revoke authentication
+
+    fun logout() { // revoke authentication
+
+        // Make a network request to log the user out
+        logoutUser()
+
+        // Clear the user's data
+//        clearUserData()
+
+        // Update the UI
+        updateUI()
+    }
+
+    private fun updateUI() {
+       // Updating UI
+    }
+
+    private fun logoutUser() {
+    /*// Send a request to the server to log the user out
+
+        val client = OkHttpClient()
+        val request = DownloadManager.Request.Builder()
+            .url("https://example.com/logout")
+            .build()
+        client.newCall(request).enqueue(object : Callback {
+             fun onFailure(call: Call, e: IOException) {
+                // Handle the error
+            }
+
+             fun onResponse(call: Call, response: Response) {
+                // The logout was successful
+            }*/
     }
 }
